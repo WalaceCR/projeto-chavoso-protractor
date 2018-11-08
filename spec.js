08e3a26e3e6e2c0181b085a84055b2cf434e403e
WalaceCR <walace.dacunha@yahoo.com.br>
2018-11-08 17:21:31 -0200

    Nova execução

--- spec.js
+++ spec.js
@@ -1,14 +1,14 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
-        'browserName': 'firefox'
+        'browserName': 'chrome'
     },
     specs: ['spec.js'],
    framework: 'jasmine2' ,
     onPrepare: function() {
         var reporters = require('/node_modules/jasmine-reporters');
         
-        jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
+        jasmine.getEnv().addReporter(reporters.JUnitXmlReporter({
             consolidateAll: true,
             savePath: '/node_modules/jasmine-reporters/testresults',
             filePrefix: 'xmloutput'
@@ -27,7 +27,7 @@ expect(guru.getText()).toEqual('Hello GURU99!');
 
 
 describe('Enter GURU99 Name with error', function() {
- it('should add a Name as GURU99', function() {
+ it('should add a Name as GURUCHANGE99', function() {
  browser.get('https://angularjs.org');
  element(by.model('yourName')).sendKeys('GURU99');
  var guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
